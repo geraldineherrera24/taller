22ead31a72b3f8fa66862c5d31badd35d9458493
geraldine herrera <gh24022016@gmail.com>
2018-09-17 10:28:10 -0500

    netbeans
    pase de netbeans al proyecto

---
+++ proyecto/NetBeansProjects/Pantallas/src/java/controlador/prueba_ruta.java
@@ -0,0 +1,127 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Usuario
+ */
+public class prueba_ruta extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet prueba_ruta</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1> PRUEBA DE RUTA " + "</h1>");
+            listarPrueba_ruta2(out);
+            out.println("</body>");
+            out.println("</html>");
+        } finally {            
+            out.close();
+        }
+    }
+    
+    
+     void listarPrueba_ruta2(PrintWriter out) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/pantalla", "root", "");
+            PreparedStatement ps = conexion.prepareStatement("SELECT * FROM prueba_ruta2");
+            ResultSet resultados = ps.executeQuery();
+            out.println("<table border='1'><tr><th>lunes</th><th>martes</th><th>miercoles</th><th>jueves</th><th>viernes</th><th>sabado</th></tr> ");
+            while(resultados.next()) {
+                String lunes = resultados.getString("lunes");
+                String martes = resultados.getString("martes");
+                String miercoles = resultados.getString("miercoles");
+                String jueves = resultados.getString("jueves");
+                String viernes = resultados.getString("viernes");
+                String sabado = resultados.getString("sabado");
+                out.println("<tr><td>"+lunes+"</td><td>"+martes+"</td><td>"+miercoles+"</td><td>"+jueves+"</td><td>"+viernes+"</td><td>"+sabado+"</td></tr>");
+                //out.println(lunes + martes + miercoles + jueves + viernes + sabado);
+            }
+            out.println("</table>");
+            conexion.close();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(prueba_ruta.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(prueba_ruta.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+     
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
